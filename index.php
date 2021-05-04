ed86a564d57bfb7b55aa6ac534e2321e77bfdcdf
serkan <s.safran84@gmail.com>
2021-05-04 08:59:12 +0200

    files added

---
+++ index.php
@@ -0,0 +1,67 @@
+<?php
+
+use Monolog\Handler\BrowserConsoleHandler;
+use Monolog\Handler\NativeMailerHandler;
+use Monolog\Logger;
+use Monolog\Handler\StreamHandler;
+require_once 'vendor/autoload.php';
+
+// create a log channel
+$log = new Logger($_GET["type"]);
+
+//$log->pushHandler(new StreamHandler('path/to/emergency.log', Logger::EMERGENCY));
+//$log->pushHandler(new StreamHandler('path/to/warning.log', Logger::WARNING));
+//$log->pushHandler(new StreamHandler('path/to/critical.log', Logger::CRITICAL));
+//$log->pushHandler(new StreamHandler('path/to/info.log', Logger::INFO));
+
+// add records to the log
+
+
+switch ($_GET["type"]){
+    case "EMERGENCY":
+        $log->pushHandler(new StreamHandler('path/to/emergency.log', Logger::EMERGENCY));
+//        $log->pushHandler(new NativeMailerHandler('pieters.tg@gmail.com', "error message", "safran84@hotmail.com"));
+        $log->pushHandler(new BrowserConsoleHandler( Logger::EMERGENCY));
+        $log->emergency($_GET["message"]);
+        break;
+    case "WARNING":
+        $log->pushHandler(new StreamHandler('path/to/warning.log', Logger::WARNING));
+        $log->pushHandler(new BrowserConsoleHandler( Logger::WARNING));
+        $log->warning($_GET["message"]);
+        break;
+    case "CRITICAL":
+        $log->pushHandler(new StreamHandler('path/to/critical.log', Logger::CRITICAL));
+//        $log->pushHandler(new NativeMailerHandler('pieters.tg@gmail.com', "error message", "safran84@hotmail.com"));
+        $log->pushHandler(new BrowserConsoleHandler( Logger::CRITICAL));
+        $log->critical($_GET["message"]);
+        break;
+    case "ERROR":
+        $log->pushHandler(new StreamHandler('path/to/critical.log', Logger::ERROR));
+//        $log->pushHandler(new NativeMailerHandler('pieters.tg@gmail.com', "error message", "safran84@hotmail.com"));
+        $log->pushHandler(new BrowserConsoleHandler( Logger::ERROR));
+        $log->error($_GET["message"]);
+        break;
+    case "ALERT":
+        $log->pushHandler(new StreamHandler('path/to/critical.log', Logger::ALERT));
+//        $log->pushHandler(new NativeMailerHandler('pieters.tg@gmail.com', "error message", "safran84@hotmail.com"));
+        $log->pushHandler(new BrowserConsoleHandler( Logger::ALERT));
+        $log->alert($_GET["message"]);
+        break;
+    case "INFO":
+        $log->pushHandler(new StreamHandler('path/to/info.log', Logger::INFO));
+        $log->pushHandler(new BrowserConsoleHandler( Logger::INFO));
+        $log->info($_GET["message"]);
+        break;
+    case "NOTICE":
+        $log->pushHandler(new StreamHandler('path/to/info.log', Logger::NOTICE));
+        $log->pushHandler(new BrowserConsoleHandler( Logger::NOTICE));
+        $log->notice($_GET["message"]);
+        break;
+    case "DEBUG":
+        $log->pushHandler(new StreamHandler('path/to/info.log', Logger::DEBUG));
+        $log->pushHandler(new BrowserConsoleHandler( Logger::DEBUG));
+        $log->debug($_GET["message"]);
+        break;
+}
+
+require_once "buttons.html";
---
+++ vendor/composer/installed.php
@@ -0,0 +1,49 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => '1.0.0+no-version-set',
+    'version' => '1.0.0.0',
+    'aliases' => 
+    array (
+    ),
+    'reference' => NULL,
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => '1.0.0+no-version-set',
+      'version' => '1.0.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => NULL,
+    ),
+    'monolog/monolog' => 
+    array (
+      'pretty_version' => '2.2.0',
+      'version' => '2.2.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '1cb1cde8e8dd0f70cc0fe51354a59acad9302084',
+    ),
+    'psr/log' => 
+    array (
+      'pretty_version' => '1.1.4',
+      'version' => '1.1.4.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd49695b909c3b7628b6289db5479a1c204601f11',
+    ),
+    'psr/log-implementation' => 
+    array (
+      'provided' => 
+      array (
+        0 => '1.0.0',
+      ),
+    ),
+  ),
+);
